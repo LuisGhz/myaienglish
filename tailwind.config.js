6e71ba29f4887909bcc4d96e0a787f04449fc859
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-24 16:39:48 -0600

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts,css}",
-  ],
+  content: ['./src/**/*.{html,ts,css}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dove-gray': {
+          50: '#F6F6F6',
+          100: '#E7E7E7',
+          200: '#D1D1D1',
+          300: '#B0B0B0',
+          400: '#888888',
+          500: '#6B6B6B',
+          600: '#5D5D5D',
+          700: '#4F4F4F',
+          800: '#454545',
+          900: '#3D3D3D',
+          950: '#262626',
+        },
+        woodsmoke: {
+          50: '#F6F6F6',
+          100: '#E7E7E7',
+          200: '#D1D1D1',
+          300: '#B0B0B0',
+          400: '#888888',
+          500: '#6D6D6D',
+          600: '#5D5D5D',
+          700: '#4F4F4F',
+          800: '#454545',
+          900: '#3D3D3D',
+          950: '#1C1C1C',
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
