a7997fb959cda332aac279beadd8743af58ee25c
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-24 17:22:39 -0600

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'translate',
+    loadComponent: () =>
+      import('./pages/translate-page/translate-page').then((m) => m.TranslatePage),
+  },
+  {
+    path: 'instructions',
+    loadComponent: () =>
+      import('./pages/instructions-page/instructions-page').then((m) => m.InstructionsPage),
+  },
+  {
+    path: '**',
+    redirectTo: 'translate',
+  },
+];
