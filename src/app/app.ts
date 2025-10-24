a4bcbcd5cc4c6fa9a890784dd8d1868b63876577
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-24 17:11:12 -0600

    Create basic layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { Sider } from './components/sider/sider';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzLayoutModule, Sider],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
