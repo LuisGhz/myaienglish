97d1e06043fd84e9d4a6317f88b1a846dde0b5f3
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-24 17:11:07 -0600

    Create sider

---
+++ src/app/components/sider/sider.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sider',
+  imports: [],
+  templateUrl: './sider.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Sider {
+
+}
