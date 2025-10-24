0a699bf8c119560d8fc75853e40b7be0bf6bdd8e
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-24 17:37:24 -0600

    Add basic inputs

--- src/app/pages/translate-page/translate-page.ts
+++ src/app/pages/translate-page/translate-page.ts
@@ -1,11 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 
 @Component({
   selector: 'app-translate-page',
-  imports: [],
+  imports: [FormsModule, NzSelectModule, NzInputModule],
   templateUrl: './translate-page.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TranslatePage {
-
+  instructions = signal<{ text: string; id: string }[]>([
+    {
+      id: 'how-to-say',
+      text: 'How to say...',
+    },
+    {
+      id: 'improve-my-writing',
+      text: 'Improve my writing',
+    },
+  ]);
 }
