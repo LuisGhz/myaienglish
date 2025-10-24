b1218d712cea6821b0a8024a13ac29f4392f6c5c
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-24 17:22:00 -0600

    Create translate and instructions pages

---
+++ src/app/pages/instructions-page/instructions-page.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instructions-page',
+  imports: [],
+  templateUrl: './instructions-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class InstructionsPage {
+
+}
---
+++ src/app/pages/translate-page/translate-page.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-translate-page',
+  imports: [],
+  templateUrl: './translate-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TranslatePage {
+
+}
