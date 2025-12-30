ca9d76b1379ec3d135c37b06c7993624adf990b3
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-30 12:13:04 -0600

    Add @Injectable

--- src/app/store/auth/auth.store.ts
+++ src/app/store/auth/auth.store.ts
@@ -1,6 +1,7 @@
 import { Action, Selector, State, StateContext } from '@ngxs/store';
 import { AuthStoreModel } from './models';
 import { AuthActions } from './auth.actions';
+import { Injectable } from '@angular/core';
 
 @State<AuthStoreModel>({
   name: 'auth',
@@ -9,6 +10,7 @@ import { AuthActions } from './auth.actions';
     token: null,
   },
 })
+@Injectable()
 export class AuthStore {
   @Selector()
   static isAuthenticated(state: AuthStoreModel): boolean {
