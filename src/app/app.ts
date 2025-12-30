7e5d05827c97c35d41c2d8d6072565b558c4bf50
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-29 21:31:59 -0600

    Add isAuthenticated

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { Sider } from './components/sider/sider';
+import { select } from '@ngxs/store';
+import { AuthStore } from './store/auth/auth.store';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +14,7 @@ import { Sider } from './components/sider/sider';
 })
 export class App {
   readonly isCollapsed = signal(false);
+  readonly isAuthenticated = select(AuthStore.isAuthenticated);
 
   onCollapsedChange(collapsed: boolean): void {
     this.isCollapsed.set(collapsed);
