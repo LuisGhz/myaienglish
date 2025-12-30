818bff52e9076afc04f93248d10c0151ebdbbc04
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-29 21:30:46 -0600

    Create translation routes

---
+++ src/app/features/translation/translation.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+
+export const TRANSLATION_ROUTES: Routes = [
+  {
+    path: 'translate',
+    loadComponent: () =>
+      import('./pages/translate-page/translate-page').then((m) => m.TranslatePage),
+  },
+  {
+    path: 'instructions',
+    loadComponent: () =>
+      import('./pages/instructions-page/instructions-page').then((m) => m.InstructionsPage),
+  },
+  {
+    path: 'favorites',
+    loadComponent: () =>
+      import('./pages/fav-translations-page/fav-translations-page').then(
+        (m) => m.FavTranslationsPage
+      ),
+  },
+];
