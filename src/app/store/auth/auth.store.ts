7dd353cf6621dde3b11b1ff63286362cd4705886
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-30 11:48:58 -0600

    Add login action

--- src/app/store/auth/auth.store.ts
+++ src/app/store/auth/auth.store.ts
@@ -1,5 +1,6 @@
-import { Selector, State } from '@ngxs/store';
+import { Action, Selector, State, StateContext } from '@ngxs/store';
 import { AuthStoreModel } from './models';
+import { AuthActions } from './auth.actions';
 
 @State<AuthStoreModel>({
   name: 'auth',
@@ -12,5 +13,13 @@ export class AuthStore {
   @Selector()
   static isAuthenticated(state: AuthStoreModel): boolean {
     return state.isAuthenticated;
+  }
+
+  @Action(AuthActions.Login)
+  login(ctx: StateContext<AuthStoreModel>, action: AuthActions.Login) {
+    ctx.setState({
+      isAuthenticated: true,
+      token: action.payload,
+    });
   }
 }
