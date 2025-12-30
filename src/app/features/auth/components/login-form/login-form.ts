b7013a8efc0299388409ea4cc5163e41f49636e8
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-30 11:49:42 -0600

    Update login form

--- src/app/features/auth/components/login-form/login-form.ts
+++ src/app/features/auth/components/login-form/login-form.ts
@@ -1,28 +1,49 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthApi } from '@auth/services';
+import { dispatch } from '@ngxs/store';
+import { AuthActions } from '@st/auth/auth.actions';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { GenFormError } from 'src/app/shared/components/gen-form-error/gen-form-error';
 
 interface LoginFormControls {
   email: FormControl<string>;
   password: FormControl<string>;
 }
 
-interface LoginFormValue {
-  email: string;
-  password: string;
-}
-
 @Component({
   selector: 'app-login-form',
-  imports: [ReactiveFormsModule, NzInputModule],
+  imports: [ReactiveFormsModule, NzInputModule, GenFormError],
   templateUrl: './login-form.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginForm {
   readonly #fb = inject(FormBuilder);
+  readonly #router = inject(Router);
+  readonly #authApi = inject(AuthApi);
+  readonly #login = dispatch(AuthActions.Login);
 
   readonly form = this.#fb.nonNullable.group<LoginFormControls>({
-    email: this.#fb.nonNullable.control(''),
-    password: this.#fb.nonNullable.control(''),
+    email: this.#fb.nonNullable.control('', [Validators.required, Validators.email]),
+    password: this.#fb.nonNullable.control('', [Validators.required]),
   });
+
+  async onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      this.form.markAllAsDirty();
+      return;
+    }
+
+    const value = this.form.getRawValue();
+    try {
+      const response = await this.#authApi.login(value);
+      this.#login(response.accessToken).subscribe(() => {
+        this.#router.navigateByUrl('/').then(() => window.location.reload());
+      });
+    } catch (error) {
+      console.error('Login failed', error);
+    }
+  }
 }
