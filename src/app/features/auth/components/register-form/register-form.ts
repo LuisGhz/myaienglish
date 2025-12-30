02c0ccbfee083456bf292e5afd7497f4d2d75e17
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-29 21:29:27 -0600

    Create register form component

---
+++ src/app/features/auth/components/register-form/register-form.ts
@@ -0,0 +1,56 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { passwordMatchValidator } from '@auth/validators/password-match.validator';
+import { strongPasswordValidator } from '@auth/validators/strong-password.validator';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { GenFormError } from 'src/app/shared/components/gen-form-error/gen-form-error';
+
+interface RegisterFormModel {
+  fullName: FormControl<string>;
+  email: FormControl<string>;
+  password: FormControl<string>;
+  confirmPassword: FormControl<string>;
+}
+
+@Component({
+  selector: 'app-register-form',
+  imports: [ReactiveFormsModule, RouterLink, NzInputModule, NzButtonModule, GenFormError],
+  templateUrl: './register-form.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RegisterForm {
+  readonly #form = inject(FormBuilder);
+  readonly formGroup = this.#form.group<RegisterFormModel>(
+    {
+      fullName: this.#form.control('', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.pattern(/\S+/), Validators.minLength(3)],
+      }),
+      email: this.#form.control('', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.pattern(/\S+/), Validators.email],
+      }),
+      password: this.#form.control('', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.pattern(/\S+/), strongPasswordValidator],
+      }),
+      confirmPassword: this.#form.control('', {
+        nonNullable: true,
+        validators: [Validators.required, Validators.pattern(/\S+/)],
+      }),
+    },
+    {
+      validators: [passwordMatchValidator('password', 'confirmPassword')],
+    },
+  );
+
+  onSubmit(): void {
+    if (this.formGroup.valid) {
+      const formValue = this.formGroup.value;
+      // Handle registration logic here
+      console.log('Registration data:', formValue);
+    }
+  }
+}
