95aebbc6a5592926540e549f5ad8ea5a22243e0a
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-29 21:32:06 -0600

    Create auth guard

---
+++ src/app/core/guards/auth-guard.ts
@@ -0,0 +1,26 @@
+import { CanActivateChildFn, CanActivateFn, Router } from '@angular/router';
+import { select } from '@ngxs/store';
+import { AuthStore } from '../../store/auth/auth.store';
+import { inject } from '@angular/core';
+
+export const authGuard: CanActivateFn = () => {
+  const router = inject(Router);
+  const isAuthenticated = select(AuthStore.isAuthenticated);
+  console.log('Auth Guard check:', isAuthenticated());
+  if (!isAuthenticated()) {
+    router.navigate(['/auth/login']);
+    return false;
+  }
+  return isAuthenticated();
+};
+
+export const authChildGuard: CanActivateChildFn = () => {
+  const router = inject(Router);
+  const isAuthenticated = select(AuthStore.isAuthenticated);
+  console.log('Auth Guard check:', isAuthenticated());
+  if (!isAuthenticated()) {
+    router.navigate(['/auth/login']);
+    return false;
+  }
+  return isAuthenticated();
+};
