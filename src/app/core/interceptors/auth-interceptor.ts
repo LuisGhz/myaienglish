3b953401b2d6652b8e69c54ea273478597f96684
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-30 20:54:52 -0600

    Handle new token

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -1,18 +1,33 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-import { select } from '@ngxs/store';
+import { HttpInterceptorFn, HttpResponse } from '@angular/common/http';
+import { dispatch, select } from '@ngxs/store';
+import { AuthActions } from '@st/auth/auth.actions';
 import { AuthStore } from '@st/auth/auth.store';
+import { tap } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = select(AuthStore.token)();
+  const updateToken = dispatch(AuthActions.UpdateToken);
 
   if (token) {
     const clonedReq = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
       },
+      withCredentials: true,
     });
     return next(clonedReq);
   }
 
-  return next(req);
+  return next(
+    req.clone({
+      withCredentials: true,
+    }),
+  ).pipe(
+    tap((response) => {
+      if (response instanceof HttpResponse && response.headers.has('x-new-jwt-token')) {
+        const newToken = response.headers.get('x-new-jwt-token');
+        if (newToken) updateToken(newToken);
+      }
+    }),
+  );
 };
