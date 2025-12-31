101d0da2b2456a6c233acceee4e781f1a3d05bad
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-30 20:50:38 -0600

    Add UpdateToken

--- src/app/store/auth/auth.actions.ts
+++ src/app/store/auth/auth.actions.ts
@@ -6,6 +6,11 @@ export namespace AuthActions {
     constructor(public payload: string) {}
   }
 
+  export class UpdateToken {
+    static readonly type = type('UpdateToken');
+    constructor(public payload: string) {}
+  }
+
   export class Logout {
     static readonly type = type('Logout');
   }
