3f03d0b482741a8fe6aef591b8e3cae3b6171898
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2026-01-02 16:28:31 -0600

    Handle new unauthorized responses

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -1,33 +1,43 @@
-import { HttpInterceptorFn, HttpResponse } from '@angular/common/http';
+import {
+  HttpInterceptorFn,
+  HttpResponse,
+  HttpErrorResponse,
+  HttpStatusCode,
+} from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthApi } from '@auth/services';
 import { dispatch, select } from '@ngxs/store';
+import { tap } from 'rxjs';
 import { AuthActions } from '@st/auth/auth.actions';
 import { AuthStore } from '@st/auth/auth.store';
-import { tap } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = select(AuthStore.token)();
   const updateToken = dispatch(AuthActions.UpdateToken);
+  const authApi = inject(AuthApi);
 
-  if (token) {
-    const clonedReq = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`,
+  const reqToSend = token
+    ? req.clone({
+        setHeaders: { Authorization: `Bearer ${token}` },
+        withCredentials: true,
+      })
+    : req.clone({ withCredentials: true });
+
+  return next(reqToSend).pipe(
+    tap({
+      next: (response) => {
+        if (response instanceof HttpResponse && response.headers.has('x-new-access-token')) {
+          const newToken = response.headers.get('x-new-access-token');
+          if (newToken) updateToken(newToken);
+        }
       },
-      withCredentials: true,
-    });
-    return next(clonedReq);
-  }
-
-  return next(
-    req.clone({
-      withCredentials: true,
-    }),
-  ).pipe(
-    tap((response) => {
-      if (response instanceof HttpResponse && response.headers.has('x-new-access-token')) {
-        const newToken = response.headers.get('x-new-access-token');
-        if (newToken) updateToken(newToken);
-      }
+      error: (err) => {
+        if (err instanceof HttpErrorResponse && err.status === HttpStatusCode.Unauthorized) {
+          try {
+            authApi.logout();
+          } catch {}
+        }
+      },
     }),
   );
 };
